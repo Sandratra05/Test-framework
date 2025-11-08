e2b0fedf182a70241e88a541d1ec50172bd5ff8c
Sandratra Rabenanahary <sandratrahasinarivelo5@gmail.com>
2025-11-08 22:02:14 +0300

    Add about.jsp file with basic HTML structure

--- src/main/java/controllers/Home.java
+++ src/main/java/controllers/Home.java
@@ -2,11 +2,19 @@ package main.java.controllers;
 
 import annotations.Controller;
 import annotations.Url;
+import view.ModelView;
 
 @Controller
 public class Home {
     @Url("/home")
     public String index() {
         return "Welcome to the Home Page!";
+    }
+
+    @Url("/about")
+    public ModelView about() {
+        ModelView mv = new ModelView();
+        mv.setView("about.jsp");;
+        return mv;
     }
 }
